0f9c69289f2d90e370ce6ce1740e165d1e7a0092
SoreyC <soreycortes@gmail.com>
2022-10-31 07:21:07 -0600

    Función Recursiva

--- functions.js
+++ functions.js
@@ -1,54 +1,28 @@
 const pathNode = require('path');
 const fs = require('fs');
 const { marked } = require('marked');
+const path = require('path');
 // const chalk = require('chalk');
 
-//let pathprueba = 'Archivos-md-txt/prueba.txt';
+let pathprueba = 'Archivos-md-txt/prueba.txt';
 
 // const pathProcess = process.argv[2];
 //  console.log('process', pathProcess);
 
-// //Verificando si existe una ruta
-//  const verifyPath = (pathProcess) =>{
-
-//     if(fs.existsSync(pathProcess)){
-//         return fileExist;
-//     } else{ 
-//         console.log('no hay archivo');
-//     }
-//     //return verifyPath;
-//     console.log('que hay?', verifyPath);
-// };
-
-
-
-//Verificando si existe directorio
-// const verifyDirectory = (path) => {
-    
-//     if(fs.readdirSync(path)){
-//         return directoryExist;
-//     } else{ 
-//         console.log('no hay directorio');
-//     }
-//     // return verifyDirectory;
-//     // console.log('que hay?', verifyDirectory);
-// }
 
 
 //resolver rutas relativas en absolutas
 // METODO QUE RESUELVE RUTA RELATIVA-ABSOLUTA path.isAbsolute = true si es absoluta. path.resolve la resuelve.
 const absolutePath = (route) => {
-    // let absolutPath;
     if (pathNode.isAbsolute(route)) {
         return route;
         // console.log('es relativa', route);
     } else {
         return pathNode.resolve(route);
-    // console.log('se cambia a absoluta', route);
+        // console.log('se cambia a absoluta', route);
     }
-    //return absolutPath
 };
-// console.log('ya es absoluta', absolutePath(path));
+//  console.log('ya es absoluta', absolutePath(path));
 
 //TODO: JSDOC
 /**
@@ -57,62 +31,88 @@ const absolutePath = (route) => {
  * @returns data
  * esta funcion se va a usar para extraer los links
  */
-// const readFilePromise = (pathProcess) => {   
-       
-//     return new Promise((resolve, reject) => {
-//         fs.readFile(path, 'utf8', (error, data) => {
+const readFilePromise = (pathprueba) => {
 
-//             if (error) {
-//                 reject(error);
-//             }
-        
-//             //aca se podria de una vez sacar los links
+    return new Promise((resolve, reject) => {
+        fs.readFile(pathprueba, 'utf8', (error, data) => {
 
-//             resolve({
-//                 path: pathProcess,
-//                 data: data,
-//             });
-//             //console.log(data);
-//         });
-//     });
+            if (error) {
+                reject(error);
+            }
 
-// };
-// readFilePromise(pathProcess)
-//     .then((res) => {
-//         console.log(res);
-//     })
-//     .catch((err) => {
-//         console.log(err);
-//     });
+            //aca se podria de una vez sacar los links
+
+            resolve({
+                path: pathprueba,
+                data: data,
+            });
+            //console.log(data);
+        });
+    });
+};
+readFilePromise(pathprueba)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 
 //Para extraer las extensiones y meterla en la funcion de la lectura de dir
 
-const extName = (path) => {
-    const ext = pathNode.extname(path);
-    return ext;
+
+// const verifyFilt = (path) => {
+
+//     if(fs.existsSync(path)){
+//         return fileExist;
+//     } else{ 
+//         console.log('no hay directorio');
+//     }
+//     // return verifyFile;
+//     // console.log('que hay?', verifyFile);
+// }
+
+
+//Verificando si existe directorio
+const getMdFiles = (path) => {
+    let mdFiles = [];
+    if (fs.statSync(path).isDirectory()) {
+        fs.readdirSync(path).forEach(file => {
+            const fileResolve = pathNode.resolve(`${path}/${file}`)
+            if (fs.statSync(fileResolve).isDirectory()) {
+                mdFiles = mdFiles.concat(getMdFiles(fileResolve))
+            }else{
+                if (pathNode.extname(file) === '.md')
+                mdFiles.push(file)
+            }         
+        });
+    } else {
+        if (pathNode.extname(path) === '.md')
+            mdFiles.push(path)
+    }
+    return mdFiles
 };
-console.log(extName('prueba.txt'));
+console.log(getMdFiles('C:/Users/LABORATORIA/OneDrive/Documentos/MDlinks/BOG005-md-links/Archivos-md-txt'));
+// console.log(getMdFiles('Archivos-md-txt/readme.md'));
 
 
 //mdLinks
 const mdLinks = (path, options) => {
     return new Promise((resolve, reject) => {
-        try{
-            const verifyAbsolut = absolutePath(path);
+        if (fs.existsSync(path))
+            //  verifyAbsolut = absolutePath(path);
+            // resolve(verifyAbsolut)
 
-            resolve(verifyAbsolut)
-         }catch(error){
             console.log('Ruta no válida');
-         }
-        
+
     })
 }
 
 mdLinks('dele.js')
-.then(res => {
-    console.log("si sirve", res);
-});
+    .then(res => {
+        console.log("si sirve", res);
+    });
 
 
 
